47179e93b692de95e613a63e6f161b48b7bc4be9
Furkan Titiz <65949007+titizfurkan@users.noreply.github.com>
2020-10-11 16:48:03 +0300

    update todo.js trash button

--- ReactJS/todo/src/components/Todo.js
+++ ReactJS/todo/src/components/Todo.js
@@ -1,32 +1,73 @@
-import React from 'react';
+import React from "react";
 
-const Todo = ({ text, todo, setTodos, todos}) => {
-    const deleteHandler = () => {
-        setTodos(todos.filter(el => el.id !== todo.id))
-    };
-    const completeHandler = () => {
-        setTodos(todos.map(item => {
-            if(item.id === todo.id){
-                return{
-                    ...item, completed : !item.completed
-                };
-            }
-            return item;
-        })
-        );
-    };
+const Todo = ({ text, todo, setTodos, todos }) => {
+//   const deleteHandler = () => {
+//     setTodos(todos.filter((el) => el.id !== todo.id));
+//     var element = document.getElementsByClassName("todo");
+//     element.className += "fall";
+//   };
 
-    return(
-        <div className={`todo ${todo.completed ? "completed" : ""}`}>
-            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
-            <button title="Bitir" onClick={completeHandler} className="complete-btn">
-                <i className="fas fa-check"></i>
-            </button>
-            <button title="Sil" onClick={deleteHandler} className="trash-btn">
-                <i className="fas fa-trash"></i>
-            </button>
-        </div>
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+  function deleteHandler(e) {
+    const item = e.target;
+
+    if (item.classList[0] === "trash-btn") {
+      // e.target.parentElement.remove();
+      const todo = item.parentElement;
+      todo.classList.add("fall");
+      //at the end
+      removeLocalTodos(todo);
+      todo.addEventListener("transitionend", (e) => {
+        todo.remove();
+      });
+    }
+    if (item.classList[0] === "complete-btn") {
+      const todo = item.parentElement;
+      todo.classList.toggle("completed");
+      console.log(todo);
+    }
+  }
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
     );
-}; 
+  };
+
+  return (
+    <div
+      className={`todo ${todo.completed ? "completed" : ""}`}
+    >
+      <li className={`todo-item`}>
+        {text}
+      </li>
+      <button title="Bitir" onClick={completeHandler} className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button title="Sil" onClick={deleteHandler} className="trash-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+};
 
 export default Todo;
